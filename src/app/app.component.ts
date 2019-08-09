a684ea8583c9f724d8a31a0b95bc147dfb98206d
Igor Chubenko <i.chubenko@dunice.net>
2019-08-09 16:34:59 +0300

    completed logic to count price

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'update-number';
+  public number = '';
+  public updatedNumber = 0;
+  public condition: number;
+  public step: number;
+
+  setCondition() {
+    this.condition = parseFloat(this.number);
+  }
+
+  setStep() {
+    if (this.updatedNumber === 0) {
+      this.step = 1;
+    } else if (this.updatedNumber > 0) {
+      this.step = Math.ceil(this.updatedNumber / 100);
+    } else if (this.updatedNumber < 0)  {
+      this.step = Math.ceil(-(this.updatedNumber) / 100);
+    }
+  }
+
+  updateNumber() {
+    this.setCondition();
+    const timer = setInterval(() => {
+      console.log('processing');
+      this.setStep();
+      if (this.updatedNumber < this.condition) {
+        if (this.updatedNumber !== this.condition) {
+          this.updatedNumber += this.step;
+          this.updatedNumber = Number(this.updatedNumber.toFixed(2));
+          if (this.updatedNumber > this.condition) {
+            this.updatedNumber = Number(this.condition.toFixed(2));
+          }
+        }
+      } else {
+        if (this.updatedNumber !== this.condition) {
+          this.updatedNumber -= this.step;
+          this.updatedNumber = Number(this.updatedNumber.toFixed(2));
+          if (this.updatedNumber < this.condition) {
+            this.updatedNumber = Number(this.condition.toFixed(2));
+          }
+        }
+      }
+      if (this.updatedNumber === Number(this.condition.toFixed(2))) {
+        clearInterval(timer);
+      }
+    }, 10);
+    this.number = '';
+  }
 }
